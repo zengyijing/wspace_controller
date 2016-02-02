9e97bfeefd70b0236ba6e5f828ace61febea247e
zengyijing <zengyijing19900106@gmail.com>
2016-02-01 23:43:16 -0500

    change wait/signal logic

--- packet_scheduler.cc
+++ packet_scheduler.cc
@@ -24,7 +24,9 @@ void PktQueue::Enqueue(const char *pkt, uint16_t len, uint32_t seq, MonotonicTim
   }
   Lock();
   while (IsFull()) {
+    UnLock();
     WaitEmpty();
+    Lock();
   }  
   char *buf = new char[len];
   memcpy(buf, pkt, len);
@@ -77,7 +79,9 @@ void ActiveList::Append(int id) {
 int ActiveList::Remove() {
   Lock();
   while (IsEmpty()) {
+    UnLock();
     WaitFill();
+    Lock();
   }
   int id = ids_.front();
   ids_.pop();
